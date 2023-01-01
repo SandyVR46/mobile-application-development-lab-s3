36126473c38245c23da7a4a3822058902e79c519
Sandra S S <41688158+SandyVR46@users.noreply.github.com>
2023-01-01 11:22:05 +0530

    login--to--new--activity

---
+++ simple--login--app/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.simpleloginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+    private TextView Info;
+    private int counter=5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Login = (Button) findViewById(R.id.btnLogin);
+        Info = (TextView) findViewById(R.id.tvInfo);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName.equals("farrenbrat")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+        else{
+            counter --;
+
+            Info.setText("No:of Attempts Left: " + String.valueOf(counter));
+            if(counter==0){
+                Login.setEnabled(false);
+            }
+        }
+    }
+
+
+}
